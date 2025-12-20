2ec19fa7969a713233375fd7c6b88204f9ac8015
Cuong Nguyen (Chris) <ntcuong777@gmail.com>
2025-12-20 15:38:55 +0700

    clean day1 2025

--- 2025/day1.clj
+++ 2025/day1.clj
@@ -8,26 +8,28 @@
        (clojure.string/split (slurp inp-file) #"\n")))
 
 ;; Part 1
-(def rotations (parse-input "/Users/ntcuong777/personal-projects/AdventOfCode/2025/day1_input.txt"))
-(def rotations-sample (parse-input "/Users/ntcuong777/personal-projects/AdventOfCode/2025/day1_sample.txt"))
+(def rotations (parse-input "2025/day1_input.txt"))
+(def rotations-sample (parse-input "2025/day1_sample.txt"))
 
 (def positions1 (reductions (fn [acc x]
-                              (mod (+ acc x 100) 100)) 50 rotations))
+                              (mod (+ acc x) 100))
+                            50
+                            rotations))
 (def part1 (count (filter #(= 0 %) positions1)))
 
 (def part2 (reductions (fn [[seen pos] x]
-                         (let [zero-rotations (abs (int (/ x 100)))
-                               reduced-x (if (< x 0)
-                                           (+ x (* zero-rotations 100))
-                                           (- x (* zero-rotations 100)))
-                               additional-zeros (if (or (and (< reduced-x 0)
+                         (let [num-passing-zeros-pre-under-100 (abs (int (/ x 100)))
+                               final-x-under-100 (if (< x 0)
+                                                   (+ x (* num-passing-zeros-pre-under-100 100))
+                                                   (- x (* num-passing-zeros-pre-under-100 100)))
+                               additional-zero? (if (or (and (< final-x-under-100 0)
                                                              (> pos 0)
-                                                             (<= pos (- reduced-x)))
-                                                        (and (> reduced-x 0)
-                                                             (>= reduced-x (- 100 pos))))
+                                                             (<= pos (- final-x-under-100)))
+                                                        (and (> final-x-under-100 0)
+                                                             (>= final-x-under-100 (- 100 pos))))
                                                   1 0)]
-                           [(+ seen zero-rotations additional-zeros)
-                            (mod (+ pos reduced-x) 100)]))
+                           [(+ seen num-passing-zeros-pre-under-100 additional-zero?)
+                            (mod (+ pos final-x-under-100) 100)]))
                        [0 50]
                        rotations))
 
