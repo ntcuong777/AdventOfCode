b39fee3f8739ed56bc8121b14247526be9663deb
Cuong Nguyen (Chris) <ntcuong777@gmail.com>
2026-01-04 19:20:14 +0700

    add day 11 2025 part 2

--- 2025/day11.clj
+++ 2025/day11.clj
@@ -25,23 +25,24 @@
           graph))
 
 (defn bfs
-  [graph start]
-  (loop [queue [start]
-         ;; visited is a map of node to distance from start
-         visited {start 0}]
-    (if (empty? queue)
-      visited
-      (let [current (first queue)
-            current-distance (get visited current)
-            neighbors (get graph current #{})
-            visited-nodes (set (keys visited))
-            new-neighbors (set/difference neighbors visited-nodes)
-            new-visited (reduce (fn [v n]
-                                  (assoc v n (inc current-distance)))
-                                visited
-                                new-neighbors)
-            new-queue (concat (rest queue) new-neighbors)]
-        (recur new-queue new-visited)))))
+  ([graph start] (bfs graph start #{}))
+  ([graph start exclude-nodes]
+   (loop [queue [start]
+          ;; visited is a map of node to distance from start
+          visited {start 0}]
+     (if (empty? queue)
+       visited
+       (let [current (first queue)
+             current-distance (get visited current)
+             neighbors (get graph current #{})
+             visited-nodes (set (keys visited))
+             new-neighbors (set/difference neighbors visited-nodes exclude-nodes)
+             new-visited (reduce (fn [v n]
+                                   (assoc v n (inc current-distance)))
+                                 visited
+                                 new-neighbors)
+             new-queue (concat (rest queue) new-neighbors)]
+         (recur new-queue new-visited))))))
 
 (defn count-in-out-degrees
   [graph]
@@ -55,39 +56,57 @@
           {}
           graph))
 
+(defn count-paths
+  ([input-graph start-node end-node]
+   (count-paths input-graph start-node end-node #{}))
+  ([input-graph start-node end-node excluded-nodes]
+   (let [reversed-graph (reverse-graph input-graph)
+         reachable-from-end (bfs reversed-graph end-node excluded-nodes)
+         reachable-from-start (bfs input-graph start-node excluded-nodes)
+         reachable-both (set/intersection (set (keys reachable-from-end))
+                                          (set (keys reachable-from-start)))
+         ;; Build reduced graph with only nodes reachable from both "you" and "out"
+         reduced-graph (reduce (fn [acc [node neighbors]]
+                                 (if (contains? reachable-both node)
+                                   (assoc acc node (set/intersection neighbors reachable-both))
+                                   acc))
+                               {}
+                               input-graph)
+         reduced-graph-degrees (count-in-out-degrees reduced-graph)]
+     (loop [queue [start-node]
+            degrees reduced-graph-degrees
+            path-counts {start-node 1}]
+       (if (empty? queue)
+         (get path-counts end-node 0)
+         (let [current (first queue)
+               current-path-count (get path-counts current 0)
+               neighbors (get reduced-graph current #{})
+               [new-degrees new-path-counts new-queue]
+               (reduce (fn [[deg pc q] neighbor]
+                         (let [updated-pc (update pc neighbor (fnil + 0) current-path-count)
+                               updated-deg (update-in deg [neighbor :in] dec)]
+                           (if (= 0 (get-in updated-deg [neighbor :in]))
+                             [updated-deg updated-pc (conj q neighbor)]
+                             [updated-deg updated-pc q])))
+                       [degrees path-counts (rest queue)]
+                       neighbors)]
+           (recur new-queue new-degrees new-path-counts)))))))
+
 (defn part-1-count-paths
   [input-graph]
-  (let [reversed-graph (reverse-graph input-graph)
-        reachable-out (bfs reversed-graph "out")
-        reachable-you (bfs input-graph "you")
-        reachable-both (set/intersection (set (keys reachable-out))
-                                         (set (keys reachable-you)))
-        ;; Build reduced graph with only nodes reachable from both "you" and "out"
-        reduced-graph (reduce (fn [acc [node neighbors]]
-                                (if (contains? reachable-both node)
-                                  (assoc acc node (set/intersection neighbors reachable-both))
-                                  acc))
-                              {}
-                              input-graph)
-        reduced-graph-degrees (count-in-out-degrees reduced-graph)]
-    (loop [queue ["you"]
-           degrees reduced-graph-degrees
-           path-counts {"you" 1}]
-      (if (empty? queue)
-        (get path-counts "out" 0)
-        (let [current (first queue)
-              current-path-count (get path-counts current 0)
-              neighbors (get reduced-graph current #{})
-              [new-degrees new-path-counts new-queue]
-              (reduce (fn [[deg pc q] neighbor]
-                        (let [updated-pc (update pc neighbor (fnil + 0) current-path-count)
-                              updated-deg (update-in deg [neighbor :in] dec)]
-                          (if (= 0 (get-in updated-deg [neighbor :in]))
-                            [updated-deg updated-pc (conj q neighbor)]
-                            [updated-deg updated-pc q])))
-                      [degrees path-counts (rest queue)]
-                      neighbors)]
-          (recur new-queue new-degrees new-path-counts))))))
+  (count-paths input-graph "you" "out"))
+
+(defn part-2-count-paths
+  [input-graph]
+  (let [svr-fft (count-paths input-graph "svr" "fft" #{"dac" "out"})
+        svr-dac (count-paths input-graph "svr" "dac" #{"fft" "out"})
+        dac-out (count-paths input-graph "dac" "out" #{"fft" "svr"})
+        fft-out (count-paths input-graph "fft" "out" #{"dac" "svr"})
+        dac-fft (count-paths input-graph "dac" "fft" #{"svr" "out"})
+        fft-dac (count-paths input-graph "fft" "dac" #{})]
+    (+ (* svr-fft fft-dac dac-out)
+       (* svr-dac dac-fft fft-out))))
 
 (def sample-input (parse-input "2025/day11_sample.txt"))
+(def sample-input2 (parse-input "2025/day11_sample2.txt"))
 (def real-input (parse-input "2025/day11_input.txt"))
